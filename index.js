2d2e5bae3e4227f2ab26fdfa29f4e6dd15238999
Manish <manishdas11811480@gmail.com>
2023-01-15 21:57:18 +0530

    Added file upload feature

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
+const { writeFile } = require('fs');
+
 const app = require('express')();
 const http = require('http').Server(app);
-const io = require('socket.io')(http, {cors: {origin: "*"}});
+const io = require('socket.io')(http, {cors: {origin: "*"},maxHttpBufferSize: 1e8 });
 const port = process.env.PORT || 3000;
 const NEW_CHAT_MESSAGE_EVENT = "newChatMessage";
+
+//maxHttpBufferSize: 1e8 // 100 MB
 
 console.log("Running")
 
@@ -19,10 +23,25 @@ io.on('connection', (socket) => {
   socket.on(NEW_CHAT_MESSAGE_EVENT, (data) => {
     io.in(roomId).emit(NEW_CHAT_MESSAGE_EVENT, data);
   });
+
+  socket.on('newFileupload', (fileObject, callback) => {
+    // console.log(fileObject)
+    const fileName = fileObject.type.split('.')[0]
+    const fileExtension = fileObject.type.split('.')[1]
+
+    writeFile(`./files/${fileName}${Math.floor(Math.random() * 900) + 10}.${fileExtension}`, fileObject.file, (err) => {
+      callback({message: err ? 'failure' : 'success'})
+    })  
+
+    //sending file back to the room
+    // io.in(roomId).emit(NEW_CHAT_MESSAGE_EVENT, data);
+
+  })
   
   socket.on("disconnect", () => {
     socket.leave(roomId);
   });
+
 //   socket.emit('connection', null);
 // socket.on('FromClient', msg => {
 //     io.emit('FromAPI', msg); //returning back to frontend
